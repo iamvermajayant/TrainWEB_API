e5297e164e7fb27f4e33b48d742d9db6b184e6db
Panindhra <phanitallapudi@gmail.com>
2023-10-13 22:00:47 +0530

    added the model for reset password

---
+++ Models/resetPassword.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApi.Models
+{
+    public class resetPassword
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        [Required]
+        public string UserEmail { get; set; }
+        [Required]
+        public int otp { get; set; }
+    }
+}
